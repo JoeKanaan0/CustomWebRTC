0c4788550ed7a1bc2d9b8c2e5925a445d4a22da0
JoeKanaan0 <joekanaan04061961@gmail.com>
2023-11-18 13:23:15 +0200

    Added Dynamic Video Grid Resizing for meetings.

--- src/Pages/Meeting/Meeting.jsx
+++ src/Pages/Meeting/Meeting.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useStream } from '../../Context/StreamContext'; 
-import useSocket from "../../Hooks/useSocket"; 
+import { useStream } from '../../Context/StreamContext';
+import useSocket from "../../Hooks/useSocket";
 import { handleMuteUnmute, handleOpenCloseCamera } from '../../Utils/Buttons'
 import './Meeting.css';
 
@@ -13,11 +13,52 @@ function Meeting() {
 
   const socket = useSocket(navigate, userVideo, setStream);
 
+  const [otherUsersCount, setOtherUsersCount] = useState(0);
+  const [gridSize, setGridSize] = useState({ rows: 1, columns: 1 });
+
+
   useEffect(() => {
     if (stream && userVideo.current) {
       userVideo.current.srcObject = stream;
     }
-  }, [stream]);
+
+    if (socket) {
+      // Fetch the user count and update the state
+      socket.on('user-count-updated', (count) => {
+        setOtherUsersCount(count - 1); // Subtract 1 for the current user
+      });
+    }
+
+  }, [stream, socket]);
+
+  useEffect(() => {
+    const updateGridSize = () => {
+      const totalUsers = otherUsersCount + 1;
+      let columns, rows;
+  
+      if (totalUsers === 1) {
+        columns = 1;
+        rows = 1;
+      } else {
+        columns = Math.min(4, totalUsers);
+        rows = Math.ceil(totalUsers / columns);
+      }
+      setGridSize({ rows, columns });
+    };
+  
+    updateGridSize();
+  }, [otherUsersCount]);
+  
+  
+
+  // Grid style based on computed rows and columns
+  const gridStyle = {
+    display: 'grid',
+    gridTemplateColumns: `repeat(${gridSize.columns}, 1fr)`,
+    gridTemplateRows: `repeat(${gridSize.rows}, 1fr)`,
+    gap: '10px',
+    padding: '5px'
+  };
 
   /**
    * Function to handle leaving the meeting.
@@ -32,19 +73,27 @@ function Meeting() {
 
   return (
     <div className="App">
-      <main>
-        {/* Video elements for displaying local and remote videos */}
-        <video ref={userVideo} id="localVideo" autoPlay muted></video>
-        <video id="remoteVideo" autoPlay></video>
-        <div>
-          {/* Buttons to control mute/unmute and camera, and to leave the call */}
-          <button onClick={() => handleMuteUnmute(stream)}>Mute/Unmute</button>
-          <button onClick={() => handleOpenCloseCamera(userVideo)}>Open/Close Camera</button>
-          <button id="callButton" onClick={handleLeave}>Leave</button>
+      <div className="main-content">
+        <div className="video-grid" style={gridStyle}>
+          {/* Local video */}
+          <video ref={userVideo} id="localVideo" autoPlay muted></video>
+
+          {/* Dynamically create video elements for other users */}
+          {[...Array(otherUsersCount)].map((_, index) => (
+            <video key={index} id={`remoteVideo-${index}`} autoPlay></video>
+          ))}
         </div>
-      </main>
+      </div>
+      <div className="control-buttons">
+        {/* Buttons to control mute/unmute and camera, and to leave the call */}
+        <button onClick={() => handleMuteUnmute(stream)}>Mute/Unmute</button>
+        <button onClick={() => handleOpenCloseCamera(userVideo)}>Open/Close Camera</button>
+        <button id="callButton" onClick={handleLeave}>Leave</button>
+      </div>
     </div>
-  );
+
+
+  )
 }
 
 export default Meeting;
